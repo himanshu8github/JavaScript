7858c43520efc0e1b50236e170245b3fe5313913
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2023-12-10 05:28:12 +0000

    operators & datatypes

---
+++ Basics1/datatypes.js
@@ -0,0 +1,54 @@
+//two data types
+
+
+//-1 prmitive(STACK)
+/*
+String
+Number
+ Boolean
+ Null
+ Undefined
+ Symbol
+ BigInt
+*/
+const firstName="Himanshu"
+const score = 200
+const age= 20
+let UserisLoggedIn= true/false 
+const nodeversion= null
+let BankBalance= undefined
+const buttonId= symbol("id")
+const aReallyLongNumber= 17747364767861873n
+
+
+//-2 Reference or (non-prmitive) - Objects (HEAP)
+
+/*
+ 
+Arrays
+Objects
+Functions
+
+*/
+
+//Array
+
+const numbers =[1,2,2,5]
+const heroes =["himani"]
+
+//objects
+
+const UserObject={
+        name: "himanshu",
+        age:20,
+        IsLoggedIn:true,
+}
+
+//Functions
+
+const SayHello = function(){
+    console.log("himanshu")
+}
+
+const RegisterAUser = function(){}
+const loginUser = function(){}
---
+++ Basics1/operator.js
@@ -0,0 +1,29 @@
+const numberOne="2"
+const numberTWO="3"
+
+const numberThree= numberOne + numberTWO
+
+ console.log(numberThree)
+
+
+//comparison operator
+
+// let pw = 3
+// let pwskills = 13
+
+// console.log(pw == pwskills)
+
+// let pw = 3
+// let pwskills = 3
+
+// console.log(pw == pwskills)
+
+let pw = 3
+let pwskills = "3"
+
+console.log(pw == pwskills)
+
+
+
+
+
