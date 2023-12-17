ef6ef1d5a81e1e0bf81fd91f5148252b439df7db
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2023-12-17 09:53:39 +0000

    Array discussion

---
+++ Basics1/myarray.js
@@ -0,0 +1,25 @@
+const myArray = [1,2,3,4,5]
+const  myNewArray = new Array (1,2,3,4,5)
+
+console.log(myNewArray[2]);
+
+const dcHeros = ["batman" , "spiderman" , "flash" , "life"]
+console.log(dcHeros.length);
+dcHeros.push("wonder woman");
+// console.log(dcHeros);
+
+// dcHeros.pop()
+// console.log(dcHeros);
+
+// dcHeros.unshift("aquaman")
+// console.log(dcHeros);
+
+// console.log(myNewArray.toString());
+// console.log(dcHeros.toString());
+// console.log(dcHeros.join("-"));
+console.log(dcHeros.slice(1,3));
+console.log(dcHeros);
+
+console.log(dcHeros.splice(1,3));
+console.log(dcHeros);
+
