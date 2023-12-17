da7bfc90f10126e20a78d174879a0e74003a4ce9
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2023-12-17 09:08:37 +0000

    String discussion

--- Basics1/numAndstring.js
+++ Basics1/numAndstring.js
@@ -1,30 +1,30 @@
-// const firstname = 'Himanshu'
-// const lastname = "HC"
+const firstname = 'Himanshu'
+const lastname = "HC"
 
-// console.log(firstname + '' + lastname);
-// console.log(`my first name iS ${firstname} and last name is ${lastname}`);
+console.log(firstname + '' + lastname);
+console.log(`my first name iS ${firstname} and last name is ${lastname}`);
 
-// const username = new String ('himanshucom')
-// console.log(username[0]);
-// console.log(username[3]);
+const username = new String ('himanshucom')
+console.log(username[0]);
+console.log(username[3]);
 
-// console.log(username[6]);
-
-
-// console.log(username.length);
-
-// console.log(username.charAt(0));
-// console.log(username.substring(0 , 5));
-// console.log(username.substring(0 , 9));
+console.log(username[6]);
 
 
-// let email = ' hc@.google  '
-// console.log(email);
+console.log(username.length);
 
-// let emailTwo = 'hc@.google'
-// console.log(emailTwo.replace('hc', 'himanshu'));
-// console.log(email.includes('hc'));
-// console.log(emailTwo.toUpperCase());
+console.log(username.charAt(0));
+console.log(username.substring(0 , 5));
+console.log(username.substring(0 , 9));
+
+
+let email = ' hc@.google  '
+console.log(email);
+
+let emailTwo = 'hc@.google'
+console.log(emailTwo.replace('hc', 'himanshu'));
+console.log(email.includes('hc'));
+console.log(emailTwo.toUpperCase());
 
 const num = new Number (5.34244)
 console.log(num.toFixed(3));
