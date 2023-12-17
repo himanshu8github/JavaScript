a093e024a5d463b2df0520765b8ab70c811877e3
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2023-12-17 06:46:06 +0000

    Conversion discussion

---
+++ Basics1/conversion.js
@@ -0,0 +1,12 @@
+let bankBalance = 'abc'
+
+let bankBalanceinInt = Number(bankBalance)
+
+console.log(bankBalance);
+console.log(bankBalanceinInt);
+
+console.log(NaN == NaN);
+
+console.log(2 == 2);
+console.log(5 + undefined);
+console.log(5 + null);
