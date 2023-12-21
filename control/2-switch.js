c89f0f1768f23f12c3971e0a362b2b80c1241e54
HIMANSHU <126444642+himanshu8github@users.noreply.github.com>
2023-12-21 08:46:41 +0000

    switch discussion

---
+++ control/2-switch.js
@@ -0,0 +1,58 @@
+// build a rating system for pizzahut
+
+// 1 star - poor
+// 2 star - emm, ok
+// 3 star - average
+// 4 star - cool
+// 5 star - great
+// 6 star - awesome
+// 7 star - nailed it
+
+
+// switch (expression){
+//     case value1:
+//         statement1;
+//         statement2;
+//         break;
+        
+//         case value2:
+//         statement3;
+//         statement4;
+//         break;
+// }
+
+let getStarFromUser = "11"
+let starInNumber = parseInt(getStarFromUser)
+
+switch ( starInNumber) {
+    case 1:
+        console.log("poor");
+        
+        break;
+
+  case 2:
+     console.log("emm , ok");
+            break;
+
+     case 3:
+         console.log("average"); 
+                break;
+
+    case 4:
+           console.log("cool");
+                    break;
+
+         case 5:
+               console.log("great");
+                        break; 
+
+         case 6:
+              console.log("awesome");
+                            break;
+
+               case 7:
+                 console.log("Nailed it !!!"); 
+                                break;
+       default:
+        console.log("i dont know what is rating");
+}
